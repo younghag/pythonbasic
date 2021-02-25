e224cdc9c8509e48f9920c54cc2a57c227a7b8c7
younghag <78770231+younghag@users.noreply.github.com>
2021-02-25 17:48:25 +0900

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+prices = []
+temps = [32.0, 212.0, 0.0, 81.6, 100., 45.3]
+words = [ 'hello', 'world' ]
+car_details = ['Toyota', 'RAV4', 2.2, 60807]
+everything = [prices, temps, words, car_details]
+odds_and_ends = [[1,2,3], ['a','b','c'], ['One', 'Two', 'Three']]
